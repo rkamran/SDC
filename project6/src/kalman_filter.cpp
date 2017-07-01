cf0ab65a212468a1462e5413130261a842491023
Rashid Kamran <rkamran@gmail.com>
2017-06-30 19:02:10 -0700

    Added new source

---
+++ project6/src/kalman_filter.cpp
@@ -0,0 +1,64 @@
+#include "kalman_filter.h"
+#include "tools.h"
+
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+
+KalmanFilter::KalmanFilter() {}
+
+KalmanFilter::~KalmanFilter() {}
+
+void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
+                        MatrixXd &H_in, MatrixXd &R_in, MatrixXd &Q_in) {
+  x_ = x_in;
+  P_ = P_in;
+  F_ = F_in;
+  H_ = H_in;
+  R_ = R_in;
+  Q_ = Q_in;
+}
+
+void KalmanFilter::Predict() {
+  /**
+  TODO:
+    * predict the state
+  */
+    x_ = F_ * x_;
+    P_ = F_ * P_ * F_.transpose() + Q_;
+}
+
+void KalmanFilter::Update(const VectorXd &z) {
+  /**
+  TODO:
+    * update the state by using Kalman Filter equations
+  */
+    VectorXd y = z - H_ * x_;
+    MatrixXd S = H_ * P_ * H_.transpose() + R_;
+    MatrixXd K = P_ * H_.transpose() * S.inverse();
+    
+    long x_size = x_.size();
+    MatrixXd I = MatrixXd::Identity(x_size, x_size);
+    
+    //New State Now
+    x_ = x_ + (K * y);
+    P_ = (I - K * H_) * P_;
+    
+}
+
+void KalmanFilter::UpdateEKF(const VectorXd &z) {
+  /**
+  TODO:
+    * update the state by using Extended Kalman Filter equations
+  */
+    VectorXd y = z - H_ * x_;
+    MatrixXd S = H_ * P_ * H_.transpose() + R_;
+    MatrixXd K = P_ * H_.transpose() * S.inverse();
+    
+    long x_size = x_.size();
+    MatrixXd I = MatrixXd::Identity(x_size, x_size);
+    
+    //New State Now
+    x_ = x_ + (K * y);
+    P_ = (I - K * H_) * P_;
+    
+}
